e79aefdb0939300321484e06409dbee8d6671d92
StephaTech <stephatech123@gmail.com>
2023-03-16 23:41:01 +0000

    try_yourself_version_Final

---
+++ app/src/main/java/com/example/mynoactivitiytest/SectionFour.kt
@@ -0,0 +1,53 @@
+package com.example.mynoactivitiytest
+
+/*fun main(){
+    //Creating instances of classes
+    var denis = Person("Denis", "Panjuta")
+    var john = Person()
+    var johnPeterson = Person(lastName = "Peterson")
+
+}
+
+class Person(firstName: String = "John", lastName: String = "Doe"){
+    //Initializer Block
+    init{
+        println("Initialized a new Person object with " +
+        "firstName = $firstName and lastName = $lastName")
+        /*WILL print
+        *Initialized a new Person object with
+        firstName = Denis and lastName = Panjuta
+        * Initialized a new Person object with
+        firstName = John and lastName = Doe
+        * Initialized a new Person object with
+        firstName = John and lastName = Peterson */
+    }
+}*/
+
+/*Please create a class called MobilePhone.
+It should have three primary constructor variables, osName, brand and model.
+
+Use the initializer to print those details.
+
+Create three objects of that class in the Main function.
+
+Examples of phones would be:
+Samsung Galaxy S20 Ultra. Here the osName is Android, brand is Samsung, model is Galaxy S20 Ultra*/
+fun main(){
+    //3 objects
+    //var SamsungGalaxyS20Ultra = MobilePhone( "Android", "Samsung", "Galaxy S Ultra")
+    var test = MobilePhone( "SamsungGalaxyS20Ultra","Android", "Samsung", "Galaxy S Ultra")
+    //var IphoneCatorze = MobilePhone("IOS", "Apple", "AppleUltra")
+    var test2 = MobilePhone("Iphone14","IOS", "Apple", "AppleUltra")
+    //var XAOMIvinte = MobilePhone("XaoChina", "Xaomi", "XaomiUltra")
+    var test3 = MobilePhone("XAOM20","XaoChina", "Xaomi", "XaomiUltra")
+    var test4 = MobilePhone()
+}
+//class and constructors
+class MobilePhone(NameMobile: String = "Huawei10", osName: String = "HarmonyOS", brand: String ="HTC", model: String = "HuaweiUltra"){
+    //Initializer Block
+    init {
+
+        println(" Santa Claus initialized my new year of 2024 with the best $NameMobile, and best operation system $osName, $brand, $model pleaseeee!!")
+    }
+
+}
---
+++ app/src/main/java/com/example/mynoactivitiytest/myTest2023.kt
@@ -0,0 +1,33 @@
+package com.example.mynoactivitiytest
+/*fun main(){
+    var name : String = "Steph"
+    name = null -> Compilation ERROR
+    var nullableName : String? = "Steph"
+    //nullableName = null
+    println("")
+
+    //First way on the past
+    var len = name.length
+    var len2 = nullableName?.length
+    nullableName?.Let { prntln(it.length)}
+
+    //this line means if nullableName has a value ex Denis them
+    //print if not print the Default Guest
+
+    //?: Elvis operator
+    val name =  nullableName ?: "Guest"
+    println("name is $name")
+
+    println(nullableName!!.lowercase())
+
+    //val wifesAge: String? = user?.wife?.age ?: 0
+
+    //println(nullableName?.lowercase())
+/* ? por o ? resume de por tudo isso que ta embaixo
+    if(nullableName != null){
+        var len2 = nullableName.length
+    }else{
+        null
+    }*/
+
+}*/
