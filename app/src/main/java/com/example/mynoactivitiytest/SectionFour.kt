80a51098013f1dd176dd872bda9e1f4c5cca65b2
StephaTech <stephatech123@gmail.com>
2023-03-17 00:16:22 +0000

    Exercise solution_version_Final

--- app/src/main/java/com/example/mynoactivitiytest/SectionFour.kt
+++ app/src/main/java/com/example/mynoactivitiytest/SectionFour.kt
@@ -32,7 +32,8 @@ Create three objects of that class in the Main function.
 
 Examples of phones would be:
 Samsung Galaxy S20 Ultra. Here the osName is Android, brand is Samsung, model is Galaxy S20 Ultra*/
-fun main(){
+
+/*fun main(){
     //3 objects
     //var SamsungGalaxyS20Ultra = MobilePhone( "Android", "Samsung", "Galaxy S Ultra")
     var test = MobilePhone( "SamsungGalaxyS20Ultra","Android", "Samsung", "Galaxy S Ultra")
@@ -50,4 +51,17 @@ class MobilePhone(NameMobile: String = "Huawei10", osName: String = "HarmonyOS",
         println(" Santa Claus initialized my new year of 2024 with the best $NameMobile, and best operation system $osName, $brand, $model pleaseeee!!")
     }
 
+}*/
+
+//Solution for Exercise Classes and Objects
+fun main(){
+    val iphone = MobilePhone("iOS", "Apple", "iPhone 12")
+    val galaxyS20 = MobilePhone("Android","Samsung", "Galaxy S20")
+    val mateXS = MobilePhone("Android", "Huawei", "Mate X S")
+}
+
+class MobilePhone(osName: String, brand: String, model: String){
+    init {
+        println("The phone $model from $brand uses $osName as its Operating System")
+    }
 }
